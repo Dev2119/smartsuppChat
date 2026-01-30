7d2719c8f1d8c43d0982b8dd31b04d6ade907f45
theAD-dev <dev@thead.com.au>
2026-01-31 00:17:32 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,49 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Official Printer Support",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* Smartsupp Chat */}
+        <Script
+          id="smartsupp-chat"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _smartsupp = _smartsupp || {};
+              _smartsupp.key = '2e37e1b81ac6f79c3b18774702c1c15a6243dfcb';
+              window.smartsupp||(function(d) {
+                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];
+                c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';
+                s.parentNode.insertBefore(c,s);
+              })(document);
+            `,
+          }}
+        />
+
+        {children}
+      </body>
+    </html>
+  );
+}
