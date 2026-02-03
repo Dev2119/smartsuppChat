dd99eeb9bdac2125f3f772ea1fc7b67cd8a6fc98
Dev2119 <developersource91@gmail.com>
2026-02-03 18:25:56 +0000

    CanonMetaUpdate1

--- app/errorcanon/page.js
+++ app/errorcanon/page.js
@@ -1,16 +1,16 @@
 "use client";
+
+import { useEffect } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import ContactPage from "./ContactPage";
-import { useEffect } from "react";
-export const metadata = {
-  title: "Error Canon Printer Support",
-};
+
+
 
 export default function SuccessPage() {
-  useEffect(() => {
-      document.title = "Error Canon Printer Support";
-    }, []);
+ useEffect(() => {
+    document.title = "Error Canon Printer Support";
+  }, []);
   return (
     <div style={{ textAlign: "center", marginTop: "0px" }}>
          <Header />
