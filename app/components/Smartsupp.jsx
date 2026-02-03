1c5e68a4bba8392af0e855395c1ab93ca6f92565
Dev2119 <developersource91@gmail.com>
2026-02-03 19:23:05 +0000

    sdfsdf

--- app/components/Smartsupp.jsx
+++ app/components/Smartsupp.jsx
@@ -4,17 +4,24 @@ import { useEffect } from "react";
 
 export default function Smartsupp() {
   useEffect(() => {
-    var _smartsupp = _smartsupp || {};
-    _smartsupp.key = "2996e5ea3ca8fbed95d0eb2aa8ef3007521fcb1e";
-    window.smartsupp || (function (d) {
-      var s, c, o = smartsupp = function () { o._.push(arguments) }; o._ = [];
-      s = d.getElementsByTagName("script")[0];
-      c = d.createElement("script");
-      c.type = "text/javascript"; c.charset = "utf-8"; c.async = true;
-      c.src = "https://www.smartsuppchat.com/loader.js?";
-      s.parentNode.insertBefore(c, s);
-    })(document);
+    window._smartsupp = window._smartsupp || {};
+    window._smartsupp.key = "2996e5ea3ca8fbed95d0eb2aa8ef3007521fcb1e";
+
+    if (!window.smartsupp) {
+      (function (d) {
+        var s, c, o = function () { (o._ = o._ || []).push(arguments) }; 
+        o._ = [];
+        window.smartsupp = o;
+        s = d.getElementsByTagName("script")[0];
+        c = d.createElement("script");
+        c.type = "text/javascript";
+        c.charset = "utf-8";
+        c.async = true;
+        c.src = "https://www.smartsuppchat.com/loader.js?";
+        s.parentNode.insertBefore(c, s);
+      })(document);
+    }
   }, []);
 
-  return null; // This component renders nothing visible
+  return null;
 }
